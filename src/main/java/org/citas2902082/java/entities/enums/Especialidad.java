ee219241aae7b9fbe0b740b4a669deafc641e5c2
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-02-29 17:34:58 -1200

    Proyecto creado

---
+++ src/main/java/org/citas2902082/java/entities/enums/Especialidad.java
@@ -0,0 +1,12 @@
+package org.citas2902082.java.entities.enums;
+
+public enum Especialidad {
+    PEDIATRIA,
+    CARDIOLOGIA,
+    ONCOLOGIA,
+    MEDICINA_GENERAL,
+    PSICOLOGIA,
+    DERMATOLOGIA,
+    LABORATORIO
+
+}
